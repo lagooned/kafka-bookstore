468959750c6f73cc384de8c8cb029b4d0186dc8e
lagooned <jared.lite@gmail.com>
2022-06-01 03:14:23 -0500

    kafka / BookSerdes / ws

--- registry/src/main/java/com/example/book/registry/BookSerdes.java
+++ registry/src/main/java/com/example/book/registry/BookSerdes.java
@@ -11,27 +11,20 @@ public class BookSerdes {
   public static Map<String, String> CONFIG_MAP = Map.of(
     JsonDeserializer.TRUSTED_PACKAGES, "com.example.book.registry",
     JsonDeserializer.VALUE_DEFAULT_TYPE, new Book().getClass().getName());
-
   public static BookJsonDeserializer BOOK_JSON_DESERIALIZER = new BookJsonDeserializer();
   public static BookJsonSerializer BOOK_JSON_SERIALIZER = new BookJsonSerializer();
   public static JsonSerde<Book> JSON = new BookSerde();
 
   public static class BookJsonDeserializer extends JsonDeserializer<Book> {
-    public BookJsonDeserializer() {
-      super(); this.configure(CONFIG_MAP, false);
-    }
+    public BookJsonDeserializer() { super(); configure(CONFIG_MAP, false); }
   }
 
   public static class BookJsonSerializer extends JsonSerializer<Book> {
-    public BookJsonSerializer() {
-      super(); this.configure(CONFIG_MAP, false);
-    }
+    public BookJsonSerializer() { super(); configure(CONFIG_MAP, false); }
   }
 
   public static class BookSerde extends JsonSerde<Book> {
-    public BookSerde() {
-      super(BOOK_JSON_SERIALIZER, BOOK_JSON_DESERIALIZER);
-    }
+    public BookSerde() { super(BOOK_JSON_SERIALIZER, BOOK_JSON_DESERIALIZER); }
   }
 
 }
