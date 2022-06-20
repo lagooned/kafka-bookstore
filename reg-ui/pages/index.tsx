f254d772757ba62c74840099a8be9940aa99bd1a
lagooned <jared.lite@gmail.com>
2022-06-20 14:49:16 -0500

    index.tsx: use @emotion/styled

--- reg-ui/pages/index.tsx
+++ reg-ui/pages/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { Box, Container, Typography } from '@mui/material';
+import styled from '@emotion/styled';
 import Meta from '../src/components/meta';
-import styles from '../styles/Home.module.css';
+
+const MainBox = styled(Box)`
+  padding: 5vmin
+`;
+
+const MainFooter = styled.footer`
+  display: flex;
+  flex: 1;
+  padding: 2rem 0;
+  border-top: 1px solid #eaeaea;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home: NextPage = () => (
   <Container maxWidth="lg">
     <Meta title="books" />
-    <Box sx={{ padding: '5vmin' }}>
+    <MainBox>
       <Typography variant="h4" component="h1" gutterBottom>
         Books
       </Typography>
-    </Box>
-    <footer className={styles.footer}>
+    </MainBox>
+    <MainFooter>
       jared engler &copy; 2022
-    </footer>
+    </MainFooter>
   </Container>
 );
 
