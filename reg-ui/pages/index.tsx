a94d00ece12cbf3fbd92d8578768b4569d266e72
lagooned <jared.lite@gmail.com>
2022-06-21 12:12:33 -0500

    pages/index.tsx: Home: pull out static components
    - memo doesn't help here
      - memo prevents rerender of children
        - not render of itself

--- reg-ui/pages/index.tsx
+++ reg-ui/pages/index.tsx
@@ -8,6 +8,19 @@ import {
 } from '@mui/material';
 import Meta from '../src/components/meta';
 
+const MainBox = styled(Box)`
+  padding: 5vmin
+`;
+
+const MainFooter = styled('footer')`
+  display: flex;
+  flex: 1;
+  padding: 2rem 0;
+  border-top: 1px solid #eaeaea;
+  justify-content: center;
+  align-items: center;
+`;
+
 type HeaderTextProps = {children: string};
 const HeaderText = memo(({ children }: HeaderTextProps) => (
   <Typography variant="h1" gutterBottom>
@@ -52,24 +65,14 @@ const BookList = ({ data }: BookListProps) => {
   );
 };
 
+const title = 'kafka-books';
+const data = [
+  { id: 'd0fbb33a-305e-43b1-b66c-a9abb0968e78', title: 'test1' },
+  { id: '9ab83066-6f98-42ab-b1fa-ca7fcb0ef7b8', title: 'test2' },
+  { id: '199a90c5-a882-402d-af2e-aa3745638f15', title: 'test3' },
+];
+
 const Home: NextPage = () => {
-  const title = 'kafka-books';
-  const MainBox = memo(styled(Box)`
-    padding: 5vmin
-  `);
-  const MainFooter = memo(styled('footer')`
-    display: flex;
-    flex: 1;
-    padding: 2rem 0;
-    border-top: 1px solid #eaeaea;
-    justify-content: center;
-    align-items: center;
-  `);
-  const data = [
-    { id: 'd0fbb33a-305e-43b1-b66c-a9abb0968e78', title: 'test1' },
-    { id: '9ab83066-6f98-42ab-b1fa-ca7fcb0ef7b8', title: 'test2' },
-    { id: '199a90c5-a882-402d-af2e-aa3745638f15', title: 'test3' },
-  ];
   return (
     <Container maxWidth="md">
       <Meta title={title} />
